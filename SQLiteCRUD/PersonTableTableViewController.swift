6fdf02dbbe17cd1515d407092937943dad16040f
Thomas Woerdeman <thomaswoerdeman@franks-Mac.local>
2016-10-17 01:46:03 -0700

    Snackbar

--- SQLiteCRUD/PersonTableTableViewController.swift
+++ SQLiteCRUD/PersonTableTableViewController.swift
@@ -32,7 +32,6 @@ class PersonTableTableViewController: UITableViewController {
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
